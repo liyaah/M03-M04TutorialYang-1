ca5e8213e2f919cc56875d67cf611742cbcdc476
Julia Yang <90215588+liyaah@users.noreply.github.com>
2022-04-19 02:47:47 +0000

    Initial commit

---
+++ SRC/App.vue
@@ -0,0 +1,72 @@
+<template>
+  <div id="main-app" class="container">
+    <div class="row justify-content-center">
+      <add-appointment @add="addItem"/>
+      <search-appointments @searchRecords="searchAppointments"/>
+      <appointment-list :appointments="searchedApts" @remove="removeItem" @edit="editItem"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import AddAppointment from "./components/AddAppointment";
+import SearchAppointments from "./components/SearchAppointments";
+import AppointmentList from "./components/AppointmentList";
+import _ from "lodash";
+import axios from "axios";
+export default {
+  name: "MainApp",
+  data: function() {
+    return {
+      appointments: [],
+      searchTerms: "",
+      aptIndex: 0
+    };
+  },
+  components: {
+    AppointmentList,
+    SearchAppointments,
+    AddAppointment
+  },
+  mounted() {
+    axios.get("./data/appointments.json").then(
+      response =>
+        (this.appointments = response.data.map(item => {
+          item.aptId = this.aptIndex;
+          this.aptIndex++;
+          return item;
+        }))
+    );
+  },
+  computed: {
+    searchedApts: function() {
+      return this.appointments.filter(item => {
+        return (
+          item.petName.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.petOwner.toLowerCase().match(this.searchTerms.toLowerCase()) ||
+          item.aptNotes.toLowerCase().match(this.searchTerms.toLowerCase())
+        );
+      });
+    }
+  },
+  methods: {
+    searchAppointments: function(terms) {
+      this.searchTerms = terms;
+    },
+    addItem: function(apt) {
+      apt.aptId = this.aptIndex;
+      this.aptIndex++;
+      this.appointments.push(apt);
+    },
+    removeItem: function(apt) {
+      this.appointments = _.without(this.appointments, apt);
+    },
+    editItem: function(id, field, text) {
+      const aptIndex = _.findIndex(this.appointments, {
+        aptId: id
+      });
+      this.appointments[aptIndex][field] = text;
+    }
+  }
+};
+</script>
